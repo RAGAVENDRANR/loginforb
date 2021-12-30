57e1b76337ea73be6e7c7e042bd1b763eeb72842
RAGAVENDRAN <52205602+RAGAVENDRANR@users.noreply.github.com>
2021-12-30 10:48:01 +0530

    first commit

--- EmployeeMgnt/src/app/app.component.ts
+++ EmployeeMgnt/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EmployeeMgnt';
-}
+  loginform = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required ,Validators.minLength(6)]),
+    // cpassword: new FormControl('', [Validators.required,Validators.minLength(6)]),
+  });
+  // Submit() {
+  //   console.log(
+  //     'USERNAME :  ' +
+  //       this.loginform.controls['username'].value +
+  //       ' ' +
+  //       'PASSWORD : ' +
+  //       this.loginform.controls['password'].value
+  //   );
+    // alert(
+    //   'USERNAME :  ' +
+    //     this.loginform.controls['username'].value + " "+" && "+
+    //     "Form submitted Sucessfully"
+    // );
+  }
+  // reseter() {
+  //   this.loginform.reset();
+  // }
+
